99a8cc3d26817715f89e72d51ee7a9b955ff1a42
Mateus Goulart Añez <mateusgoulart20@hotmail.com>
2023-09-14 16:33:26 -0400

    vai dar tudo certo

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { LoginCadastro } from "./pages/LoginCadastro/LoginCadastro.jsx";
 import { Esttistica } from "./pages/Esttistica/Esttistica.jsx";
-import { Perfil } from "./pages/Perfil/Perfil.jsx";
+import { Login } from "./pages/Login.jsx";
 
 
 export function Navigations() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={( <Perfil /> )} />
+                <Route path="/" element={( <Login /> )} />
             </Routes>
         </BrowserRouter>
     )
