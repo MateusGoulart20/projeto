3bbe431a90b01d392f4e9f45de94b82e81a3c0f3
Mateus Goulart Añez <mateusgoulart20@hotmail.com>
2023-09-24 18:53:42 -0400

    localhost to 0.0.0.0

--- api/src/database/config/config.js
+++ api/src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     username: 'postgres',
     password: 'postgres',
-    host: 'localhost',
+    host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 5432,
     database: 'nodejs',
     define: { logging: false }
