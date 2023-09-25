073c704e6b2a1b4705116a7adb3294b3827a9e5d
Mateus Goulart Añez <mateusgoulart20@hotmail.com>
2023-09-24 21:57:07 -0400

    database_url adicionada ao config do banco

--- api/src/database/config/config.js
+++ api/src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     username: 'postgres',
     password: 'postgres',
-    host: '0.0.0.0',
+    host: env("DATABASE_URL"),
     port: process.env.PORT ? Number(process.env.PORT) : 5432,
     database: 'nodejs',
     define: { logging: false }
